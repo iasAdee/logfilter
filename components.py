69615d9f8d7b8ecd266897b1748400772afc0736
iasAdee <37335201+iasAdee@users.noreply.github.com>
2024-10-29 23:32:24 +0500

    Update components.py

--- components.py
+++ components.py
@@ -39,9 +39,10 @@ next_date = current_date + timedelta(days=1)
 
 nav_content = html.Div([
 
-    html.H2("LogFilter"),
+    html.Hr(style={'backgroundColor': 'yellow'}),
+    html.H2("LogFilter",style={'color': 'firebrick'}),
 
-    html.Hr(style={'backgroundColor': 'azure'}),
+    html.Hr(style={'backgroundColor': 'yellow'}),
 
     dcc.Upload(
         id='upload-data',
@@ -56,8 +57,10 @@ nav_content = html.Div([
             'borderWidth': '1px',
             'borderStyle': 'dashed',
             'borderRadius': '5px',
+            'borderColor': 'yellow',
             'textAlign': 'center',
-            'margin': '10px'
+            'margin': '10px',
+            'color':'firebrick'
         },
         # Allow multiple files to be uploaded
         multiple=True
@@ -65,28 +68,37 @@ nav_content = html.Div([
 
     html.H6(id="status"),
 
-    html.Button('Start', id='update-button', n_clicks=None),
-    html.Hr(style={'backgroundColor': 'azure'}),
+    html.Button('Start', id='update-button', n_clicks=None,style={'backgroundColor': 'yellow'}),
+    html.Hr(style={'backgroundColor': 'yellow'}),
 
 
     #navigation controls
-    html.H5("Navigation"),
-    html.A("Pal & PU Tabelle", href="#pal",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Pal & PU charts", href="#palc",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Spedition Tabelle", href="#sped",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Spedition Charts", href="#spedc",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Vereint Data Tabelle", href="#ver",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Vereint data Charts", href="#verc",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
-    html.A("Neue Update Charts", href="#neue",style={'color': 'azure'}),
-    html.Hr(style={'backgroundColor': 'azure'}),
 
-    dcc.Link('New Filter', href='/',style={'color': 'azure'}),
+    html.H5("Navigation",style={'color': 'firebrick'}),
+    html.Hr(style={'backgroundColor': 'yellow'}),
+    html.A("Pal & PU Tabelle", href="#pal",style={'color': 'azure','fontSize':"12px"}),
+    html.Br(),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.A("Pal & PU charts", href="#palc",style={'color': 'azure','fontSize':"12px"}),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.Br(),
+    html.A("Spedition Tabelle", href="#sped",style={'color': 'azure','fontSize':"12px"}),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.Br(),
+    html.A("Spedition Charts", href="#spedc",style={'color': 'azure','fontSize':"12px"}),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.Br(),
+    html.A("Vereint Data Tabelle", href="#ver",style={'color': 'azure','fontSize':"12px"}),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.Br(),
+    html.A("Vereint data Charts", href="#verc",style={'color': 'azure','fontSize':"12px"}),
+    #html.Hr(style={'backgroundColor': 'yellow'}),
+    html.Br(),
+    html.A("Neue Update Charts", href="#neue",style={'color': 'azure','fontSize':"12px"}),
+    html.Hr(style={'backgroundColor': 'yellow'}),
+
+
+    dcc.Link('New Filter', href='/page1',style={'color': 'yellow','fontWeight': 'bold'}),
     
 ])
 
