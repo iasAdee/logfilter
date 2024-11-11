a4ba330268edd74f61fe4164e00c138d82e9f492
iasAdee <37335201+iasAdee@users.noreply.github.com>
2024-11-11 13:00:02 +0500

    Update components.py
    changes few comments

--- components.py
+++ components.py
@@ -4,33 +4,7 @@ from dash import dcc, html
 from dash import Dash, dcc, html, dash_table, Input, Output, State, callback
 from datetime import datetime, timedelta
 
-"""dropdown_options2 = [
-    {'label': '訂製片', 'value': 'fig1'},
-    {'label': '試賣', 'value': 'fig2'},
-    {'label': '拋棄式', 'value': 'fig3'}
-]
-"""
 
-"""dcc.Dropdown(id='search-input',
-                             multi=False,
-                             placeholder='Select Sale Type...',
-                             options=dropdown_options2,
-                             value='fig1', disabled=False, style={'color': 'black'}),
-            """
-
-"""html.H5("Select Input Feature files"),
-        dcc.Slider(
-            id='edge-slider',
-            min=1,
-            max=3,
-            step=1,
-            value=1,
-            marks={
-                1: {'label': '30'},
-                2: {'label': '60'},
-                3: {'label': '90'}
-            }
-        ),"""
 
 
 current_date = datetime.now().date()
