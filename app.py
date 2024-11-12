ae6e97c9d35613ab1b79003ae977c2538cc39c96
iasAdee <37335201+iasAdee@users.noreply.github.com>
2024-11-12 11:49:00 +0500

    Update app.py

--- app.py
+++ app.py
@@ -698,7 +698,7 @@ app.layout = html.Div([
 )
 def display_page(pathname,id_, pass_):
 
-	if(id_ == "log" and pass_ == "#C3asar!"):
+	if(id_ == "log" and pass_ == "C3asar!"):
 	
 	    if pathname == '/page-2':
 	        return {'display': 'block'}, {'display': 'none'} ,{'display': 'none'}, ""
