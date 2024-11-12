3744b99488e85c10cbefdb7935da00f175119867
iasAdee <37335201+iasAdee@users.noreply.github.com>
2024-11-12 11:27:21 +0500

    Update app.py
    updated for new csvs

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def parse_contents(contents, filename, date):
         if 'csv' in filename:
             # Assume that the user uploaded a CSV file
             df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
+                io.StringIO(decoded.decode('utf-16')), delimiter='\t')
         elif 'xls' in filename:
             # Assume that the user uploaded an excel file
             df = pd.read_excel(io.BytesIO(decoded))
