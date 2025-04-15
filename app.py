1eecdd9c5ddfbd9e4654075074d72d5beb3ee7a6
iasAdee <37335201+iasAdee@users.noreply.github.com>
2025-04-15 21:50:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -214,6 +214,9 @@ def update_graph(data, n_clicks):
 
 		df["Summe von BrGew_Offen"] = ls
 
+
+		df = df[df["Fakturasperre"].isna()].reset_index(drop=True)
+
 		#print(df.columns)
 		data_preprocessor2 = DataPreprocessing(df)
 		data_req, ls, fig = data_preprocessor2.clac_2()
@@ -262,6 +265,8 @@ def update_graph2(data, n_clicks):
 
 		col_updates = {key.lower(): value for key, value in col_updates.items()}
 		df.columns = [col_updates.get(col.lower().strip(), col) for col in df.columns]
+
+		df = df[df["Fakturasperre"].isna()].reset_index(drop=True)
 
 		data_preprocessor2 = DataPreprocessing(df)
 		ls2, ls3, data1, data2, fig2, fig3, fig4, fig5, fig6, fig7, fig10, fig20 = data_preprocessor2.get_calculated_results(input=True)
@@ -559,7 +564,7 @@ container_materials = {
 }
 
 
-# Modified document processing function
+"""# Modified document processing function
 def load_docx_and_print_tables(file_content, full_string, kgs, kgs2):
     try:
         # Create a file-like object from the uploaded content
@@ -591,6 +596,60 @@ def load_docx_and_print_tables(file_content, full_string, kgs, kgs2):
         modified_file.seek(0)
         return modified_file.getvalue()
     
+    except Exception as e:
+        print(f"Error processing document: {e}")
+        return None"""
+
+def load_docx_and_print_tables(file_content,full_string, kgs, kgs2, target_row_number=None):
+    try:
+        # Load the document
+        doc_file = io.BytesIO(file_content)
+        document = Document(doc_file)
+
+
+        # Check if there are any tables in the document
+        if not document.tables:
+            print("No tables found in this document.")
+            return
+
+        # Iterate through each table in the document
+        
+        #print(document.tables)
+        print("Tables:")
+        table_count = 0
+        for i, table in enumerate(document.tables):
+            print(f"\nTable {i + 1}:")  # Add 1 to i for user-friendly indexing
+            check = False
+            count = 0
+            for row in table.rows:
+                for cell in row.cells:
+                    
+                    #print(cell.text)
+                    if(cell.text.startswith("14")):
+                        
+                        table_count +=1 
+                        print(table_count)
+                        check = True
+                    if(check == True and cell.text.strip() == ""):
+                        #print(cell.text)
+                        count+=1
+                        if(count == 1 or count == 5):
+                            continue
+                        elif(count == 2):
+                            cell.text = full_string[table_count-1]
+                        elif(count == 3):
+                            cell.text = kgs[table_count-1]
+                        elif(count == 4):
+                            cell.text = kgs2[table_count-1]
+                            
+
+            print("-" * 20)  # Separator for readability
+            
+        modified_file = io.BytesIO()
+        document.save(modified_file)
+        modified_file.seek(0)
+        return modified_file.getvalue()
+
     except Exception as e:
         print(f"Error processing document: {e}")
         return None
@@ -633,65 +692,121 @@ def store_word_template(contents, filename):
     prevent_initial_call=True
 )
 def make_pdf_from_excel(data, word_template, n_clicks):
-    if n_clicks is None or not data:
-        return "Bitte erst Daten hochladen ", None
-    
-    # Convert data to DataFrame
-    excel_data = pd.DataFrame(data)
-    
-    if len(excel_data) == 0:
-        return "Keine validen Daten gefunden  ", None
-    
-    # Process Excel data
-    full_string = ""
-    kgs = ""
-    kgs2 = ""
-    
-    for i in range(len(excel_data)):
-        first_kg = str(excel_data["Brutto"][i])
-        first_kg_char = str(excel_data["Gewichtseinheit"][i])
-        second_kg = str(excel_data["Netto"][i])
-        second_kg_char = str(excel_data["Gewichtseinheit"][i])
-        third = excel_data["Benennung"][i]
-        fourth = excel_data["UN-Nr."][i]
-        get_data = excel_data["UN-Homologation"][i]
-        zero_word = excel_data["Menge"][i]
-        tech = excel_data["Tech.Benennung 1"][i]
-        Flammpunkt = excel_data["Flammpunkt"][i]
+	if n_clicks is None or not data:
+	    return "Bitte erst Daten hochladen ", None
 
-        if pd.isna(get_data) or pd.isna(Flammpunkt):
-            continue
-        else:
-            first_word = container_materials.get(str(get_data)[1], "Unknown Material")
-            second_word = container_types.get(int(str(get_data)[0]), "Unknown Type")
-            data_to_add = (f"{zero_word} {first_word} {second_word} ({get_data})\n"
-                          f"{fourth} {third} {tech}\n{Flammpunkt}\n\n")
-            full_string += data_to_add
+	# Convert data to DataFrame
+	excel_data = pd.DataFrame(data)
 
-        kgs += f"{first_kg}{first_kg_char}\n\n\n"
-        kgs2 += f"{second_kg}{second_kg_char}\n\n\n"
+	if len(excel_data) == 0:
+	    return "Keine validen Daten gefunden  ", None
 
-    # Process Word document
-    if word_template is None:
-        # Use default template if none uploaded
-        try:
-            with open("IMO Manuell.docx", "rb") as f:
-                word_content = f.read()
-        except FileNotFoundError:
-            return "Kein Word Dokument verfügbar ", None
-    else:
-        # Use uploaded template
-        content_type, content_string = word_template.split(',')
-        word_content = base64.b64decode(content_string)
+	# Process Excel data
+	full_string = ""
+	kgs = ""
+	kgs2 = ""
+	count = 0
+	list_of_strings = []
+	list_of_kgs = []
+	list_of_kgs2 = []
 
-    modified_docx_bytes = load_docx_and_print_tables(word_content, full_string, kgs, kgs2)
-    
-    if modified_docx_bytes:
-        return "Dokument erfolgreich geladen", dcc.send_bytes(
-            modified_docx_bytes,
-            "Processed_Document.docx"
-        )
-    return "Error processing document", None
+
+	for i in range(len(excel_data)):
+	    
+	    first_kg = str(excel_data["Brutto"][i])
+	    first_kg_char = str(excel_data["Gewichtseinheit"][i])
+	    
+	    
+	    second_kg = str(excel_data["Netto"][i])
+	    second_kg_char = str(excel_data["Gewichtseinheit"][i])
+	    
+	    third = excel_data["Benennung"][i]
+	    fourth = excel_data["UN-Nr."][i]
+	    
+	    if(pd.isna(fourth)):
+	        continue
+	    
+	    get_data = excel_data["UN-Homologation"][i]
+	    zero_word = excel_data["Menge"][i]
+	    tech = excel_data["Tech.Benennung 1"][i]
+	    Flammpunkt = excel_data["Flammpunkt"][i]
+	    
+	    
+	    if(pd.isna(get_data) or pd.isna(Flammpunkt)):
+	        first_word = container_materials[get_data[1]]
+	        second_word = container_types[int(get_data[0])]
+	        data_to_add = str(zero_word)+" "+str(first_word)+" "+\
+	                        str(second_word)+""+\
+	                        str(fourth)+" "+str(third)+" "+str(tech) +"\n\n"
+	    else:
+	        first_word = container_materials[get_data[1]]
+	        second_word = container_types[int(get_data[0])]
+	        data_to_add = str(zero_word)+" "+str(first_word)+" "+\
+	                        str(second_word)+" ("+str(get_data)+")\n"+\
+	                        str(fourth)+" "+str(third)+" "+str(tech) +"\n"+str(Flammpunkt)+"\n\n"
+	        
+	        
+	        
+	        full_string += data_to_add
+	        
+	            
+	    
+	    kgs += first_kg+" "+first_kg_char+"\n\n\n\n\n"
+	    kgs2 += second_kg+" "+second_kg_char+"\n\n\n\n\n"
+	    
+	    
+	    count += 1
+	    if(count == 3 or i == len(excel_data)-1):
+	        list_of_strings.append(full_string)
+	        full_string = ""
+	        list_of_kgs.append(kgs)
+	        kgs = ""
+	        list_of_kgs2.append(kgs2)
+	        kgs2 = ""
+	        count=0
+
+
+	docx_file_path1 = 'layouts/IMO_Layout_new.docx'
+	docx_file_path2 = 'layouts/IMO_Layout_2.docx'
+	docx_file_path3 = 'layouts/IMO_Layout_3.docx'  
+	docx_file_path4 = 'layouts/IMO_Layout_4.docx'  
+	docx_file_path5 = 'layouts/IMO_Layout_5.docx'  
+	# Process Word document
+	if word_template is None:
+	    # Use default template if none uploaded
+	    try:
+	    	if(len(list_of_strings) == 1):
+		        with open(docx_file_path1, "rb") as f:
+		            word_content = f.read()
+	    	if(len(list_of_strings) == 2):
+		        with open(docx_file_path2, "rb") as f:
+		            word_content = f.read()
+	    	if(len(list_of_strings) == 3):
+		        with open(docx_file_path3, "rb") as f:
+		            word_content = f.read()
+	    	if(len(list_of_strings) == 4):
+		        with open(docx_file_path4, "rb") as f:
+		            word_content = f.read()
+	    	if(len(list_of_strings) == 5):
+		        with open(docx_file_path5, "rb") as f:
+		            word_content = f.read()
+		            		            		            
+
+	    except FileNotFoundError:
+	        return "Kein Word Dokument verfügbar ", None
+	else:
+	    # Use uploaded template
+	    content_type, content_string = word_template.split(',')
+	    word_content = base64.b64decode(content_string)
+
+	modified_docx_bytes = load_docx_and_print_tables(word_content, list_of_strings, list_of_kgs, list_of_kgs2)
+
+	if modified_docx_bytes:
+	    return "Dokument erfolgreich geladen", dcc.send_bytes(
+	        modified_docx_bytes,
+	        "Processed_Document.docx"
+	    )
+	return "Error processing document", None
 
 
 
@@ -1043,6 +1158,8 @@ def update_output(n_clicks, input_value,data):
 
 		#print(df)
 		#print(input_value)
+
+		df = df[df["Fakturasperre"].isna()].reset_index(drop=True)
 		
 		data_preprocessor2 = DataPreprocessing(df)
 		fig =data_preprocessor2.make_customized_plot(input_value)
