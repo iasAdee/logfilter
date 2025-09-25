d96c46a257e7d61607aea9dbee8515b21054a089
iasAdee <37335201+iasAdee@users.noreply.github.com>
2025-09-25 11:28:09 +0500

    Update de30.py

--- de30.py
+++ de30.py
@@ -14,7 +14,11 @@ page_11_layout = html.Div(
 
 	html.Div([
 	html.Div(
+			children=[
 			html.H6("Results"),
+			html.Button('Tabelle speichern', id='de30button', n_clicks=None,style={'backgroundColor': '#F5B323','fontSize':"12px"}),
+			dcc.Download(id="download-dataframe-de30"),
+			dcc.Store(id='de30_results'),]
 			),
 
 		html.Div(
